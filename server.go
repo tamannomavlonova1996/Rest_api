efed93ae9fe18de32e59bbdbd3b30f822b904ba5
Tamanno <t.rozikova@team.alif.tj>
2022-07-08 12:14:55 +0500

    Server

--- server.go
+++ server.go
@@ -3,27 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
-func home_page(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "Go is super easy")
+type User struct {
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
+}
+
+func (u User) getallInfo() string {
+	return fmt.Sprintf("User name is %s, he age is %d. He has money %d.", u.Name, u.Age, u.Money)
+}
+func (u *User) setName(newName string) {
+	u.Name = newName
 
 }
-func farrukh_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Farukhchik one love")
-	fmt.Println(r)
-}
-func farrukhclass_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hey? how are you")
 
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"Football", "Skate", "Dance"}}
+	//bob.setName("Alex")
+	//fmt.Fprintf(page, bob.getallInfo())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
+}
+func contacts_page(page http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(page, "Contacts page")
 }
 func handleRequest() {
 	http.HandleFunc("/", home_page)
-	http.HandleFunc("/farukh", farrukh_page)
-	http.HandleFunc("/fa", farrukhclass_page)
+	http.HandleFunc("/contacts/", contacts_page)
 	http.ListenAndServe(":1010", nil)
 }
 func main() {
+	//var bob User = ...
+
 	handleRequest()
 
 }
